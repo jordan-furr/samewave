15faf1368a4e62bdbbc39f61cc752ae19e3638b6
jordan-furr <jordanfurr98@gmail.com>
2025-09-18 15:46:42 -0400

    blue grid

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
               </li>
             ))}
           </div>
-          <div className="infoHalf">
+          <div className="infoHalf desktop">
             <p className="mb3">Samewave 7 is the collective of Artists Katrina Stalcup and Jordan Furr. They design and create quilted goods (blankets, wearables, posters, table runners, coasters), clothing, paper media, creative mail, stuffed animal accesories, needlework, a variety of embellishments, and lately, beading.</p>
             <p className="mb3">Following their 50% input / 50% output practice, Samewave 7 is propelled by an ongoing search for inspiration (exhibitions, film, books, culture and design conversations in NYC) and sustained by consistent hours in the studio.</p>
             <p className="mb5">Samewave 7 particpates in trades with other creatives and has sold goods for USD in the past. If the project is curious and open to creative control, commissions may be on the table.</p>            
